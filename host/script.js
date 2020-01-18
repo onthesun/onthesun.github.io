c7448ed9f729eb77297f2437073191475afa8a36
Onda, Koji <onthecozy@gmail.com>
2020-01-18 16:52:14 +0900

    呼び出し位置変更

--- host/script.js
+++ host/script.js
@@ -4,18 +4,6 @@ let localStream = null;
 let peer = null;
 let existingCall = null;
 
-navigator.mediaDevices.getUserMedia({video: false, audio: true})
-    .then(function (stream) {
-    // Success
-    //$('#my-video').get(0).srcObject = stream;
-    localStream = stream;
-    //const call = peer.call('y-pax_proto', localStream);
-    }).catch(function (error) {
-    // Error
-    console.error('mediaDevice.getUserMedia() error:', error);
-    return;
-    });
-
 peer = new Peer({
     key: 'ef82b5ff-e40f-4a82-8b04-c0f6e61d902c',
     debug: 3
@@ -29,9 +17,19 @@ while (true) {
   }
 }
 
+navigator.mediaDevices.getUserMedia({video: false, audio: true})
+    .then(function (stream) {
+        // Success
+        localStream = stream;
+        const call = peer.joinRoom('y-pax_proto', {mode: 'mesh', stream: localStream});
+        setupCallEventHandlers(call);
+    }).catch(function (error) {
+        // Error
+        console.error('mediaDevice.getUserMedia() error:', error);
+        return;
+    });
+
 peer.on('open', function(){
-    const call = peer.joinRoom('y-pax_proto', {mode: 'mesh', stream: localStream});
-    setupCallEventHandlers(call);
 });
 
 peer.on('error', function(err){
@@ -45,8 +43,6 @@ peer.on('disconnected', function(){
 });
 
 peer.on('call', function(call){
-    //call.answer(localStream);
-    //setupCallEventHandlers(call);
 });
 
 function setupCallEventHandlers(call){
