a3189817ef228d4975ab27a56d950d3665b65fa8
Onda, Koji <onthecozy@gmail.com>
2020-01-18 14:06:59 +0900

    Mixer 側のコード整理

--- mixer/script.js
+++ mixer/script.js
@@ -4,18 +4,6 @@ let localStream = null;
 let peer = null;
 let existingCall = null;
 
-//navigator.mediaDevices.getUserMedia({video: false, audio: true, audioReceiveEnabled: true})
-//    .then(function (stream) {
-//	// Success
-//        $('#my-video').get(0).srcObject = stream;
-//        localStream = stream;
-//    }).catch(function (error) {
-//        // Error
-//        console.error('mediaDevice.getUserMedia() error:', error);
-//        return;
-//    });
-
-//peer = new Peer('y-pax_proto', {
 peer = new Peer({
     key: 'ef82b5ff-e40f-4a82-8b04-c0f6e61d902c',
     debug: 3
@@ -37,73 +25,43 @@ peer.on('close', function(){
 peer.on('disconnected', function(){
 });
 
-//$('#make-call').submit(function(e){
-//    e.preventDefault();
-//    const call = peer.call($('#callto-id').val(), localStream);
-//    setupCallEventHandlers(call);
-//});
-
-//$('#end-call').click(function(){
-//    existingCall.close();
-//});
-
-//peer.on('call', function(call){
-//    call.answer(localStream);
-//    setupCallEventHandlers(call);
-//});
-
 function setupCallEventHandlers(call){
     if (existingCall) {
 	existingCall.close();
     };
 
     existingCall = call;
-    setupEndCallUI();
-    //$('#room-id').text(call.name);
+
     $('#their-id').text(call.name);    
 
     call.on('stream', function(stream){
-	addVideo(stream);
+	addAudio(call,stream);
+	setupEndCallUI();
     });
 
     call.on('peerLeave', function(peerId){
-	removeVideo(peerId);
+	removeAudio(peerId);
     });
 
     call.on('close', function(){
-	removeAllRemoteVideos();
+	removeAudio(call.remoteId);
 	setupMakeCallUI();
     });
-//    existingCall = call;
-
-//    call.on('stream', function(stream){
-//	addVideo(call,stream);
-//	setupEndCallUI();
-//	//$('#their-id').text(call.remoteId);
-//	$('#their-id').text(call.name);
-//    });
-
-//    call.on('close', function(){
-//	//removeVideo(call.remoteId);
-//	removeVideo(call.name);
-//	setupMakeCallUI();
-//    });
 }
 
-function addVideo(stream){
-//    $('#their-video').get(0).srcObject = stream;
-    const videoDom = $('<video autoplay>');
-    videoDom.attr('id',stream.peerId);
-    videoDom.get(0).srcObject = stream;
-    $('.pure-g').append(videoDom);
+function addAudio(stream){
+    const audioDom = $('<audio autoplay>');
+    audioDom.attr('id',stream.remoteId);
+    audioDom.get(0).srcObject = stream;
+    $('.pure-g').append(audioDom);
 }
 
-function removeVideo(peerId){
-//    $('#their-video').get(0).srcObject = undefined;
+function removeAudio(peerId){
+    $('#'+peerId).remove();
 
 }
 
-function removeAllRemoteVideos(){
+function removeAllRemoteAudios(){
     $('.pure-g').empty();
 }
 
